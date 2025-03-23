1eded72d24c26f3879d011c8ef0e16d6bf8b44be
nexer1234 <77268699+Pro666Pro@users.noreply.github.com>
2025-03-23 23:45:27 +0900

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,6 +22,12 @@ local SimonSay = Window:MakeTab({
 	PremiumOnly = false
 })
 
+local Other = Window:MakeTab({
+	Name = "Other",
+	Icon = "rbxthumb://type=Asset&id=71615493070800&w=150&h=150",
+	PremiumOnly = false
+})
+
 QuestionNumber = 0
 for i,v in pairs(workspace.RepressedMemories.RepressedMemoriesNPC.Head.Dialog:FindFirstChild("DialogChoice") and workspace.RepressedMemories.RepressedMemoriesNPC.Head.Dialog.DialogChoice:FindFirstChild("DialogChoice"):GetDescendants()) do
 if v then
@@ -38,4 +44,11 @@ SimonSay:AddParagraph("Question: Simon says jump off the map to claim your rewar
 SimonSay:AddParagraph("Question: Simon says if the reward is what you seek, You must dance on top of the Right/Left staircase Crystal.","Answer: Dance on top of the Crystals according to the NPC's Perspective by using the command ''/e dance'' in chat. If it says, ''Left Crystal'', You go and Dance on the Right Crystal (Its Left will be your Right and its Right will be your Left). Walk through the portal if it doesn't say ''Simon says''")
 SimonSay:AddParagraph("Question: Simon says riddle me this and speak it out. What has four fingers and a thumb but isn't alive?","Answer: Type ''a glove'' in chat.")
 SimonSay:AddParagraph("Simon says riddle me this and speak it out. What has a bottom at the top?","Type ''a leg'' in chat.")
+
+Other:AddButton({
+	Name = "Close cheat sheet",
+	Callback = function()
+OrionLib:Destroy()
+	 end
+})    
 end
